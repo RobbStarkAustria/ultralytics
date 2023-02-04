b2a0bb907d20183412f42404f08c8cf8add2548e
olaf <olaf.nowitzki@web.de>
2023-02-04 18:11:11 +0100

    ongoing training

--- my_predict_seils.py
+++ my_predict_seils.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 
-model = YOLO("diamant_notes_seils/vmax_epochs_m_model_SGD/weights/best.pt")
+model = YOLO("diamant_notes_seils/max_epochs_m_model/weights/best.pt")
 
 results = model.predict(
     source="seils_test/",
-    device=0,
+    # device=0,
     imgsz=1024,
-    batch=4,
+    # batch=4,
     # conf=0.7,
     # iou=0.1,
     max_det=1000,
@@ -14,7 +14,8 @@ results = model.predict(
     save=True,
     save_txt=True,
     save_conf=True,
-    line_thickness=1,
+    line_thickness=2,
     hide_labels=True
     # agnostic_nms=True
 )
+warte = ""
