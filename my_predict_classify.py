d26631d908b11830246b84d6e7df196012fead97
olaf <olaf.nowitzki@web.de>
2023-02-18 22:50:34 +0100

    ongoing classification training

--- my_predict_classify.py
+++ my_predict_classify.py
@@ -1,9 +1,13 @@
 from ultralytics import YOLO
+import torch
 
-model = YOLO("runs/classify/train4/weights/best.pt")
+model = YOLO("runs/classify/train7/weights/best.pt")
 result = model(
-    "test_images/clef_early_music_online_d177_00230.jpg",
+    "test_images/sm-d",
     save=True
     
 )
+names = model.names
+value = torch.argmax(result[0].probs)
+print(names[value.item()])
 warte = ""
