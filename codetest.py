c6e3316cd9307534c595fd2ad9c1dc6b26d9879c
olaf <olaf.nowitzki@web.de>
2024-06-16 07:53:27 +0200

    update codetest

--- codetest.py
+++ codetest.py
@@ -1,24 +1,10 @@
+import ultralytics
 from ultralytics import YOLO
-from PIL import Image
-import cv2
 
-model1 = YOLO("diamant_notes/correct-boxes/weights/best.pt")
+ultralytics.checks()
 
-# im1 = Image.open("test_images/alberti_dalmio_A.jpg")
-im1 = cv2.imread("test_images/bertani_4.jpg")
-# im2 = cv2.imread("test_images/correggio_mentreil_A.jpg")
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
-# results1 = model1.predict(source=[im1, im2], save=True, line_thickness=1)
+# Use the model
+model.train(data='coco.yaml', epochs=3, auto_augment=None, batch=32)  # train the model
 
-model2 = YOLO("diamant_notes/correct-boxes/weights/best.pt")
-
-results2 = model2.predict(source=[im1],
-                          save=True,
-                          line_thickness=1,
-                          imgsz=1024,
-                          #   conf=0.25,
-                          #   iou=0,
-                          save_txt=True,
-                          save_conf=True,
-                          )
-warte = ""
