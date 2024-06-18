4b2ba87497b03ee35a5079186e6b49b816d78ae3
olaf <olaf.nowitzki@web.de>
2024-06-18 15:44:06 +0200

    codetest

--- codetest.py
+++ codetest.py
@@ -6,5 +6,5 @@ ultralytics.checks()
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data='coco.yaml', epochs=3, auto_augment=None, batch=32)  # train the model
+model.train(data='coco8.yaml', epochs=3, auto_augment=None, batch=32)  # train the model
 
